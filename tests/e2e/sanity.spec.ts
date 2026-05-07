1de4cfca06534a83b9f5321e154bcdb0118efa47
chuisiufai <nano122090@gmail.com>
2026-05-07 09:57:19 +0100

    chore: set up vitest + playwright harness

---
+++ tests/e2e/sanity.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+test('homepage loads', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.locator('h1')).toContainText('CONSTRUCTIVIST');
+});
